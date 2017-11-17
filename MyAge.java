604c69558841e347df03dc9bd8efe8f24dfbd33d
sae1 <sae1@iastate.edu>
2017-11-16 21:11:01 -0600

    Add new file

---
+++ MyAge.java
@@ -0,0 +1,14 @@
+/**
+ *  
+ * @author Stephanie Engelhardt
+ *
+ */
+
+/**
+ * 
+ * To be implemented by the Animal class. 
+ *
+ */
+public interface MyAge{
+	int myAge();	// return age of the animal
+}
