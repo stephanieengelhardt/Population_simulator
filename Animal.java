853b520ec806e8a38611a5f800eb2da5514eda3f
sae1 <sae1@iastate.edu>
2017-11-16 21:06:21 -0600

    Add new file

---
+++ Animal.java
@@ -0,0 +1,20 @@
+/**
+ *  
+ * @author Stephanie Engelhardt
+ *
+ */
+
+/*
+ * This class is to be extended by the Badger, Fox, and Rabbit classes. 
+ */
+public abstract class Animal extends Living implements MyAge{
+	protected int age;   // age of the animal 
+
+	@Override
+	/**
+	 * @return age of the animal 
+	 */
+	public int myAge(){
+		return age; 
+	}
+}
